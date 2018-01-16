65c8b1b8d0540b6388df010ffb6add972d49160b
agniedom <34281253+agniedom@users.noreply.github.com>
2018-01-16 23:05:53 +0100

    item list

--- app/src/main/java/com/example/ela/kawiarnia/Main2Activity.java
+++ app/src/main/java/com/example/ela/kawiarnia/Main2Activity.java
@@ -1,13 +1,34 @@
 package com.example.ela.kawiarnia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Main2Activity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
+
+
+        //Lista miejsc
+        ArrayList<Cofee> cofees = new ArrayList<>();
+        cofees.add(new Cofee("Latte",12.00));
+        cofees.add(new Cofee("Cappucino",14.30));
+        cofees.add(new Cofee("Expresso",7.90));
+        cofees.add(new Cofee("Machiato",22.00));
     }
 }
--- app/src/main/java/com/example/ela/kawiarnia/MainActivity.java
+++ app/src/main/java/com/example/ela/kawiarnia/MainActivity.java
@@ -1,9 +1,45 @@
 package com.example.ela.kawiarnia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import butterknife.BindView;
+import butterknife.OnClick;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txt_edittext)
+    EditText Edit_Cofee ;
+
+    @BindView(R.id.btn_wybierzdanie)
+    Button btn_wybierzdanie ;
+
+    @BindView(R.id.btn_zamow)
+    Button btn_zamow;
+
+    @BindView(R.id.txt_longtex)
+    TextView txt_longtex;
+
+    @BindView(R.id.txt_welcomtocafee)
+    TextView txt_welcometocafee;
+
+    @OnClick(R.id.btn_wybierzdanie)
+    void onClick1(View view){
+
+        Intent intent = new Intent( MainActivity.this, Main2Activity.class);
+        Bundle bundle = new Bundle();
+        String wpisanyTekst = Edit_Cofee.getText().toString();
+        bundle.putString("KEY", wpisanyTekst);
+        intent.putExtras(bundle);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
